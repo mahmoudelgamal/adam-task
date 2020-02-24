d585ece8744de2bd22261d36942a42a9c7305d69
Mahmoud Elgamal <Mahmoud Elgamal>
2020-02-24 02:10:07 +0200

    feature list, contact us footer

--- js/main.js
+++ js/main.js
@@ -3,3 +3,31 @@ $('.navigation a, .collapse-btn').click(function () {
   $('.navigation__nav').toggleClass('open');
   $('.icon').toggleClass('close_')
 });
+
+//start text area counter
+(function countDown() {
+  let ourMassege = $('.massege'),
+      maxText = $('textarea').attr('maxlength');
+  $('textarea').keyup(function(){
+    let texetLength = $('textarea').val().length;
+    let remChars = maxText - texetLength;
+    ourMassege.html('<span>' + remChars +' characters remaining </span>');
+    $('.massege span').css({
+      color:'red',
+      fontWeight: 'bold'
+    })
+  })
+})();
+
+// dynamic tabs
+$('.feature__tabs .tab__item').on('click', (function () {
+  $(this).addClass('active-tab').siblings().removeClass('active-tab');
+  $('.feature__tabs-content > div').hide();
+  $($(this).data('content')).fadeIn()
+}));
+
+// smooth scroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
